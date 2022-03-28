8d725722bec0f20e9fe8996cee5ccbbca023cc67
Josh Anness <annessjosh@gmail.com>
2022-03-28 15:06:55 -0400

    Added Location Permissions

--- app/src/main/java/com/example/checkpoint/MainActivity.kt
+++ app/src/main/java/com/example/checkpoint/MainActivity.kt
@@ -1,13 +1,24 @@
 package com.example.checkpoint
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.mapbox.android.core.permissions.PermissionsListener
 import com.mapbox.maps.*
 import com.mapbox.maps.ResourceOptionsManager
+import com.example.checkpoint.LocationPermissionsHelper
+import com.example.checkpoint.LocationPermissionsHelper.Companion.areLocationPermissionsGranted
 
-var mapView: MapView? = null
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), PermissionsListener {
+
+    var mapView: MapView? = null
+    //private lateinit var mapView: MapView
+    private val permissionsHelper = LocationPermissionsHelper(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +26,49 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         mapView = findViewById(R.id.mapView)
         mapView?.getMapboxMap()?.loadStyleUri(Style.MAPBOX_STREETS)
+
+        if (areLocationPermissionsGranted(this)) {
+            requestPermissionIfNotGranted(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        } else {
+            permissionsHelper.requestLocationPermissions(this)
+        }
+    }
+
+    override fun onExplanationNeeded(permissionsToExplain: MutableList<String>?) {
+        Toast.makeText(
+            this,
+            "This app needs location and storage permissions in order to show its functionality.",
+            Toast.LENGTH_LONG
+        ).show()
+    }
+
+    override fun onPermissionResult(granted: Boolean) {
+        if (granted) {
+            requestPermissionIfNotGranted(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        } else {
+            Toast.makeText(
+                this,
+                "You didn't grant location permissions.",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    private fun requestPermissionIfNotGranted(permission: String) {
+        val permissionsNeeded: MutableList<String> = ArrayList()
+        if (
+            ContextCompat.checkSelfPermission(this, permission) !=
+            PackageManager.PERMISSION_GRANTED
+        ) {
+            permissionsNeeded.add(permission)
+            ActivityCompat.requestPermissions(
+                this,
+                permissionsNeeded.toTypedArray(),
+                10
+            )
+        } else {
+            //
+        }
     }
 
     override fun onStart() {
