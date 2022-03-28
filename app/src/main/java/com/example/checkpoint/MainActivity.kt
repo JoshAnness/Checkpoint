389753415a010aa423f73defdc30e8dc3c6b234c
Josh Anness <annessjosh@gmail.com>
2022-03-28 14:19:30 -0400

    Simple Map Basis

--- app/src/main/java/com/example/checkpoint/MainActivity.kt
+++ app/src/main/java/com/example/checkpoint/MainActivity.kt
@@ -1,244 +1,40 @@
 package com.example.checkpoint
 
-import android.content.DialogInterface
-import android.location.Location
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.CameraUpdateFactory.newLatLng
-import com.mapbox.android.core.location.LocationEngine
-import com.mapbox.android.core.location.LocationEngineListener
-import com.mapbox.android.core.location.LocationEnginePriority
-import com.mapbox.android.core.location.LocationEngineProvider
-import com.mapbox.android.core.permissions.PermissionsListener
-import com.mapbox.android.core.permissions.PermissionsManager
-import com.mapbox.api.directions.v5.models.DirectionsResponse
-import com.mapbox.geojson.Point
-import com.mapbox.mapboxsdk.Mapbox
-import com.mapbox.mapboxsdk.annotations.MarkerOptions
-import com.mapbox.mapboxsdk.maps.MapView
-import com.mapbox.mapboxsdk.maps.MapboxMap
-import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerPlugin
-import com.mapbox.mapboxsdk.plugins.locationlayer.modes.CameraMode
-import com.mapbox.mapboxsdk.plugins.locationlayer.modes.RenderMode
-import com.mapbox.mapboxsdk.annotations.Marker
-import com.mapbox.mapboxsdk.geometry.LatLng
-import com.mapbox.services.android.navigation.ui.v5.NavigationLauncher
-import com.mapbox.services.android.navigation.ui.v5.NavigationLauncherOptions
-import com.mapbox.services.android.navigation.ui.v5.route.NavigationMapRoute
-import com.mapbox.services.android.navigation.v5.navigation.NavigationRoute
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import com.mapbox.maps.*
+import com.mapbox.maps.ResourceOptionsManager
 
-class MainActivity : AppCompatActivity(), PermissionsListener, LocationEngineListener, MapboxMap.OnMapClickListener {
+var mapView: MapView? = null
 
-    private lateinit var mapView: MapView
-    private lateinit var map: MapboxMap
-    private lateinit var startButton: Button
-    private lateinit var permissionManager: PermissionsManager
-    private lateinit var originLocation: Location
-    private lateinit var originPosition: Point
-    private lateinit var destinationPosition: Point
-
-    private var locationEngine : LocationEngine? = null
-    private var locationLayerPlugin: LocationLayerPlugin? = null
-    private var destinationMarker: Marker? = null
-    private var navigationMapRoute: NavigationMapRoute? = null
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        ResourceOptionsManager.getDefault(this, defaultToken = getString(R.string.mapbox_access_token))
         setContentView(R.layout.activity_main)
-        Mapbox.getInstance(applicationContext, getString(R.string.access_token))
         mapView = findViewById(R.id.mapView)
-        startButton = findViewById(R.id.startButton)
-        mapView.onCreate(savedInstanceState)
-        mapView.getMapAsync {
-            mapboxMap -> map = mapboxMap
-            enableLocation()
-        }
-
-        startButton.setOnClickListener {
-            val options = NavigationLauncherOptions.builder()
-                .origin(originPosition)
-                .destination(destinationPosition)
-                //.shouldSimulateRoute(true)
-                .build()
-            NavigationLauncher.startNavigation(this, options)
-
-        }
+        mapView?.getMapboxMap()?.loadStyleUri(Style.MAPBOX_STREETS)
     }
 
-    private fun enableLocation() {
-        if(PermissionsManager.areLocationPermissionsGranted(this)) {
-            initializeLocationEngine()
-            initializeLocationLayer()
-        } else {
-            permissionManager = PermissionsManager(this)
-            permissionManager.requestLocationPermissions(this)
-        }
-    }
-
-    @SuppressWarnings("MissingPermission")
-    private fun initializeLocationEngine() {
-        locationEngine = LocationEngineProvider(this).obtainBestLocationEngineAvailable()
-        locationEngine?.priority = LocationEnginePriority.HIGH_ACCURACY
-        locationEngine?.activate()
-
-        val lastLocation = locationEngine?.lastLocation
-        if(lastLocation != null) {
-            originLocation = lastLocation
-            setCameraPosition(lastLocation)
-        } else {
-            locationEngine?.addLocationEngineListener(this)
-        }
-    }
-
-    @SuppressWarnings("MissingPermission")
-    private fun initializeLocationLayer() {
-        locationLayerPlugin = LocationLayerPlugin(mapView, map, locationEngine)
-        locationLayerPlugin?.setLocationLayerEnabled(true)
-        locationLayerPlugin?.cameraMode = CameraMode.TRACKING
-        locationLayerPlugin?.renderMode = RenderMode.NORMAL
-    }
-
-    private fun setCameraPosition(location: Location) {
-        map.animateCamera(com.mapbox.mapboxsdk.camera.CameraUpdateFactory.newLatLngZoom(LatLng(location.latitude, location.longitude), 13.0))
-    }
-
-    override fun onMapClick(point: LatLng) {
-        destinationMarker?.let {
-            map.removeMarker(it)
-        }
-        destinationMarker = map.addMarker(MarkerOptions().position(point))
-        destinationPosition = Point.fromLngLat(point.longitude, point.latitude)
-        originPosition = Point.fromLngLat(originLocation.longitude, originLocation.latitude)
-        getRoute(originPosition, destinationPosition)
-        startButton.isEnabled = true
-        startButton.setBackgroundResource(R.color.mapboxBlue)
-    }
-
-    //If user does not have location enabled
-    override fun onExplanationNeeded(permissionsToExplain: MutableList<String>?) {
-        /*val dialogBuilder = AlertDialog.Builder(this)
-        dialogBuilder.setMessage("We need your location to give you the correct directions!")
-            .setCancelable(false)
-            .setPositiveButton("Proceed", DialogInterface.OnClickListener {
-                dialog, id -> enableLocation()
-            })
-            .setNegativeButton("Cancel", DialogInterface.OnClickListener {
-                dialog, id -> dialog.cancel()
-            })
-        val alert = dialogBuilder.create()
-
-        alert.setTitle("Location Permission")
-
-        alert.show()*/
-    }
-
-    override fun onPermissionResult(granted: Boolean) {
-        if (granted) {
-            enableLocation()
-        }
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        permissionManager.onRequestPermissionsResult(requestCode, permissions, grantResults)
-    }
-
-    @SuppressWarnings("MissingPermission")
-    override fun onConnected() {
-        locationEngine?.requestLocationUpdates()
-    }
-
-    override fun onLocationChanged(location: Location?) {
-        location?.let {
-            originLocation = location
-            setCameraPosition(location)
-        }
-    }
-
-    private fun getRoute(origin: Point, destination: Point) {
-        NavigationRoute.builder()
-            .accessToken(Mapbox.getAccessToken())
-            .origin(origin)
-            .destination(destination)
-            .build()
-            .getRoute(object: Callback<DirectionsResponse> {
-                override fun onResponse(
-                    call: Call<DirectionsResponse>,
-                    response: Response<DirectionsResponse>
-                ) {
-                    val body = response.body() ?: return
-                    if (body.routes().count() == 0) {
-                        Log.e("MainActivity", "No route found")
-                        return
-                    }
-
-                    if (navigationMapRoute != null) {
-                        navigationMapRoute?.removeRoute()
-                    } else {
-                        navigationMapRoute = NavigationMapRoute(null, mapView, map)
-                    }
-
-                    navigationMapRoute?.addRoute(body.routes().first())
-                }
-
-                override fun onFailure(call: Call<DirectionsResponse>, t: Throwable) {
-                    Log.e("MainActivity", "Error: ")
-                }
-            })
-    }
-
-    @SuppressWarnings("MissingPermission")
     override fun onStart() {
         super.onStart()
-        if (PermissionsManager.areLocationPermissionsGranted(this)) {
-            locationEngine?.requestLocationUpdates()
-            locationLayerPlugin?.onStart()
-        }
-        mapView.onStart()
-    }
-
-    override fun onResume() {
-        super.onResume()
-        mapView.onResume()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        mapView.onDestroy()
-        locationEngine?.deactivate()
-    }
-
-    override fun onPause() {
-        super.onPause()
-        mapView.onPause()
+        mapView?.onStart()
     }
 
     override fun onStop() {
         super.onStop()
-        locationEngine?.removeLocationUpdates()
-        locationLayerPlugin?.onStop()
-        mapView.onStop()
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        mapView.onSaveInstanceState(outState)
+        mapView?.onStop()
     }
 
     override fun onLowMemory() {
         super.onLowMemory()
-        mapView.onLowMemory()
+        mapView?.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView?.onDestroy()
     }
 
 }
